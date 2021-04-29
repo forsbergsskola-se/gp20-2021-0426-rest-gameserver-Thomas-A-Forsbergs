dd89b143034502a5ecafc941a761de7a4209d30f
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2021-04-29 12:12:55 +0200

    Adds IndexOfString

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -6,17 +6,21 @@ namespace TinyBrowser {
     public static class Program {
         static void Main(string[] arguments) {
             // Connect to acme.com Server via TCP
-            var hostName = "example.com";
-            var port = 80;
+            const string hostName = "example.com";
+            const int port = 80;
             var tcpClient = new TcpClient(hostName, port);
             var stream = tcpClient.GetStream();
             // Get the bytes for our HTTP Request
-            var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n");
+            var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: "+hostName+"\r\n\r\n");
             // Send them over TCP
             stream.Write(bytes);
-            var buffer = new byte[1024*2];
-            var response = stream.Read(buffer, 0, buffer.Length);
-            Console.Write("Server said: "+Encoding.ASCII.GetString(buffer));
+            var buffer = new byte[1024*2+10];
+            var streamRead = stream.Read(buffer, 0, buffer.Length);
+            var getStringBuffer = Encoding.ASCII.GetString(buffer);
+            Console.Write("Server said: "+getStringBuffer);
+            const string indexOfString = "<title>";
+            // ReSharper disable once StringIndexOfIsCultureSpecific.1
+            Console.WriteLine("Index Of: "+indexOfString+getStringBuffer.IndexOf(indexOfString));
         }
     }
 }
