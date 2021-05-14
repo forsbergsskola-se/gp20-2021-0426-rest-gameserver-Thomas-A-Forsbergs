a0a48b7d42fadd5acb937b6a0b1a2e3046a0179b
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2021-05-14 14:53:00 +0200

    More classmates scripts

--- TinyBrowser/Marcus/Program_Marcus.cs
+++ TinyBrowser/Marcus/Program_Marcus.cs
@@ -6,23 +6,23 @@ using System.Text;
 
 namespace TinyBrowser.Marcus {
     internal static class Program {
-        static void Main() {
-            var tcpClient = new TcpClient();
-            const string webSite = "www.acme.com";
-            tcpClient.Connect(webSite, 80);
-            var stream = tcpClient.GetStream();
-            var send = Encoding.ASCII.GetBytes("GET / HTTP/1.1" + Environment.NewLine +
-                                               "Host: acme.com" + Environment.NewLine + Environment.NewLine);
-            stream.Write(send, 0, send.Length);
-
-            var sr = new StreamReader(stream);
-            var str = sr.ReadToEnd();
-            //Console.WriteLine(str);
-            tcpClient.Close();
-            stream.Close();
-
-            MainLoop(str);
-        }
+        // static void Main() {
+        //     var tcpClient = new TcpClient();
+        //     const string webSite = "www.acme.com";
+        //     tcpClient.Connect(webSite, 80);
+        //     var stream = tcpClient.GetStream();
+        //     var send = Encoding.ASCII.GetBytes("GET / HTTP/1.1" + Environment.NewLine +
+        //                                        "Host: acme.com" + Environment.NewLine + Environment.NewLine);
+        //     stream.Write(send, 0, send.Length);
+        //
+        //     var sr = new StreamReader(stream);
+        //     var str = sr.ReadToEnd();
+        //     //Console.WriteLine(str);
+        //     tcpClient.Close();
+        //     stream.Close();
+        //
+        //     MainLoop(str);
+        // }
 
         static string Navigator(string subDir) {
             var tcpClient = new TcpClient();
